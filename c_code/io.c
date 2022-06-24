0c4393fb23a3ffb708562832265967fe47f15018
kevinferg <kevinferg98@gmail.com>
2022-06-24 09:30:51 -0400

    Formatting, and fix static FILE* declaration

--- c_code/io.c
+++ c_code/io.c
@@ -8,17 +8,13 @@
 #include "particle_stuff.h"
 #include "vec_stuff.h"
 
-
-
-
 static int count_numbers(FILE* f);
 static int next_number(FILE* f, double *val);
 
-
-int io_load_txt(MDSystem* sys,const char* filename){
+int io_load_txt(MDSystem* sys,const char* filename) {
 	FILE* f;
 	f = fopen(filename, "r");
-	if (f==NULL){
+	if (f == NULL) {
 		fprintf(stderr,"Could not open the input file: %s\n",filename);
 		return -2;
 	}
@@ -31,7 +27,7 @@ int io_load_txt(MDSystem* sys,const char* filename){
 	
 	numbers = count_numbers(f);
 
-	if ((numbers<0) || (numbers%3)){
+	if ((numbers<0) || (numbers%3)) {
 		fprintf(stderr,"Could not get xyz coordinates from input file: %s\n",filename);
 		return -3;
 	}
@@ -41,15 +37,15 @@ int io_load_txt(MDSystem* sys,const char* filename){
 	
 	sys_zero_all(sys);
 
-	for (i=0;i<(numbers/3);i++){
-		for (j=0;j<3;j++){
-			status = next_number(f,&(position.V[j]));
+	for (i=0; i<(numbers/3); i++){
+		for (j=0; j<3; j++){
+			status = next_number(f, &(position.V[j]));
 			if (status<0){
 				fprintf(stderr,"An error ocurred while reading the file: %s\n",filename);
 				return status;
 			}
 		}
-		pt_set_pos(&(sys->particles[i]),&position);
+		pt_set_pos(&(sys->particles[i]), &position);
 	}
 	
 	fclose(f);
@@ -57,64 +53,64 @@ int io_load_txt(MDSystem* sys,const char* filename){
 }
 
 
-static int count_numbers(FILE* f){
+static int count_numbers(FILE* f) {
 	int N = 0;
-	char current=0;
+	char current = 0;
 	
-	do{
+	do {
 		current = fgetc(f);
-		if (IS_NUM_CHAR(current)){
+		if (IS_NUM_CHAR(current)) {
 			while(IS_NUM_CHAR(current))
 				current = fgetc(f);
 			N++;
 		}
-	}while (current != EOF);
+	} while (current != EOF);
 	rewind(f);
 	return N;
 }
 
-static int next_number(FILE* f, double* val){
-	char current=0;
+static int next_number(FILE* f, double* val) {
+	char current = 0;
 	static char buf[NUM_CHARS_TO_BUFFER];
 	int index = 0;
 	int status;
 	do{
 		current = fgetc(f);
-		if (current == EOF){
+		if (current == EOF) {
 			return -3;
 		}
-	}while(!IS_NUM_CHAR(current));
+	} while(!IS_NUM_CHAR(current));
 	
 	
-	while(IS_NUM_CHAR(current)){
+	while(IS_NUM_CHAR(current)) {
 		buf[index] = current;
 		index++;
 		current = fgetc(f);
-		if (index>=NUM_CHARS_TO_BUFFER)
+		if (index >= NUM_CHARS_TO_BUFFER)
 			break;
 		
 	}
 	buf[index] = '\0';
-	status = sscanf(buf,"%lf",val);
+	status = sscanf(buf, "%lf", val);
 	if (status==EOF)
 		return -3;
 	return 0;
 }
 
 
-int io_export_pdb(MDSystem* sys,const char* filename){
+int io_export_pdb(MDSystem* sys,const char* filename) {
 	FILE *f;
 	
 	f = fopen(filename, "w");
-	if (f==NULL){
+	if (f==NULL) {
 		fprintf(stderr,"Could not open the output file: %s\n",filename);
 		return -2;
 	}
 	int i;
 	
-	for(i=0;i<sys->N_particles;i++){
+	for(i=0;i<sys->N_particles;i++) {
 		fprintf(f,"ATOM%7d  C   PRO A%4d    %8.3f%8.3f%8.3f  1.00 10.00           C\n",
-		i,1,
+		i, 1,
 		sys->particles[i].pos.x,
 		sys->particles[i].pos.y,
 		sys->particles[i].pos.z);
@@ -125,17 +121,17 @@ int io_export_pdb(MDSystem* sys,const char* filename){
 }
 
 
-int io_export_xyz(MDSystem* sys,const char* filename){
+int io_export_xyz(MDSystem* sys, const char* filename){
 	FILE *f;
 	
 	f = fopen(filename, "w");
-	if (f==NULL){
+	if (f==NULL) {
 		fprintf(stderr,"Could not open the output file: %s\n",filename);
 		return -2;
 	}
 	int i;
 	fprintf(f,"%d\n",sys->N_particles);
-	for(i=0;i<sys->N_particles;i++){
+	for(i=0;i<sys->N_particles;i++) {
 		fprintf(f,"\nC %f %f %f",
 		sys->particles[i].pos.x,
 		sys->particles[i].pos.y,
@@ -146,13 +142,13 @@ int io_export_xyz(MDSystem* sys,const char* filename){
 	return 0;
 }
 
-int anim_export_frame(MDSystem* sys){
+int anim_export_frame(MDSystem* sys) {
 	FILE* f = sys->anim_file;
 	int i;
 
 	fprintf(f,"%d\n%d\n",sys->N_particles,sys->time_steps/sys->anim_every);
 
-	for(i=0;i<sys->N_particles;i++){
+	for(i=0;i<sys->N_particles;i++) {
 		fprintf(f,"%d %f %f %f\n",i,
 		sys->particles[i].pos.x,
 		sys->particles[i].pos.y,
@@ -163,16 +159,16 @@ int anim_export_frame(MDSystem* sys){
 	return 0;
 }
 
-int anim_init(MDSystem* sys, int every, const char* filename){
-	if (every < 1){
+int anim_init(MDSystem* sys, int every, const char* filename) {
+	if (every < 1) {
 		every = 1;
 	}
 	
-	static FILE* f;
+	FILE* f;
 	
 	f = fopen(filename, "w");
-	if (f==NULL){
-		fprintf(stderr,"Could not open the input file: %s\n",filename);
+	if (f==NULL) {
+		fprintf(stderr, "Could not open the input file: %s\n", filename);
 		return -2;
 	}
 
