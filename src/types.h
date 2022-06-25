2f41f203e5a978b07c514d1de116d6277b42c357
kevinferg <kevinferg98@gmail,com>
2022-06-25 14:13:47 -0400

    Improve documentation

--- src/types.h
+++ src/types.h
@@ -5,24 +5,23 @@
 
 typedef unsigned int uint;
 
-
 /* Vector with 3 components
 Can refer to components as xyz or as indices
 For example: vec.x == vec.V[0] */
-typedef union{
+typedef union {
 	struct{double x,y,z;};
 	double V[3];
-}Vec3;
+} Vec3;
 
 
 /* One particle with position, velocity, current acting force, and mass*/
-typedef struct{
-	Vec3 pos;
-	Vec3 vel;
-	Vec3 force;
-	Vec3 trupos;
-	double mass;
-}Particle;
+typedef struct {
+	Vec3 pos;       // Position of particle
+	Vec3 vel;       // Velocity of particle
+	Vec3 force;     // Current force acting on particle
+	Vec3 trupos;    // Position w.r.t. original location, ignoring PBC's
+	double mass;    // Mass of particle
+} Particle;
 
 
 /* Function pointer that takes in a distance and an array of parameters as arguments, for potential/force calculations*/
@@ -31,56 +30,53 @@ typedef double (*dist_fun) (double, double*);
 
 /* A potential, contains the functions used for force/potential calculations, 
 as well as the parameters needed, like epsilon and sigma in Lennard Jones*/
-typedef struct{
-	dist_fun U_fun;
-	dist_fun F_fun;
+typedef struct {
+	dist_fun U_fun;    // Pointer to potential (U) function
+	dist_fun F_fun;    // Pointer to force (F = -dU/dr) function
 	
-	double r_cut;
-	double U_cut;
-	double F_cut;
+	double r_cut;      // User-specified cutoff radius
+	double U_cut;      // Internally-generated U at cutoff
+	double F_cut;      // Internally-generated F at cutoff
 	
-	double* params;
-}Potential;
+	double* params;    // Pointer to an array of potential parameters.
+} Potential;
 
 
 
 /* Contains all information about an MD system 
 Particle list, number of particles, whether particles have been set up;
-Pair list, number of pairs, whether pairs have been set up;
 The potential to be used for every pair of particles;
 Number of time steps run so far;
 Size of each time step, in dimensionless time units;
 */
-typedef struct{
-	long time_steps;
-	double dt;
-	double runtime;
+typedef struct {
+	long time_steps;     // Number of time steps run so far 
+	double dt;           // Time step in dimensionless units
+	double runtime;      // Cumulative time it took simulation to run
 	
-	Vec3 boxlen;
-	Vec3 xyz_min;
-	Vec3 xyz_max;
-	double virial;
+	Vec3 boxlen;         // xyz-components of box side length
+	Vec3 xyz_min;        // Lower bounds of box
+	Vec3 xyz_max;        // Upper bounds of box
+	double virial;       // Current virial component of pressure
 	
-	int N_particles;
-	Potential* potential;
-	Particle* particles;
-	int particles_initialized;
+	int N_particles;            // Number of particles
+	Potential* potential;       // Pointer to potential acting between all particle pairs
+	Particle* particles;        // Pointer to particle array
+	int particles_initialized;  // Whether particles are initialized
 	
-	int anim_initialized;
-	int anim_every;
-	FILE *anim_file;
+	int anim_initialized;    // Whether trajectory animation file has been initialized
+	int anim_every;          // Add particle snapshot to trajectory every _ frames
+	FILE *anim_file;         // Pointer to trajectory animation file
 	
-	int log_initialized;
-	int log_every;
-	FILE *log_file;
+	int log_initialized;     // Whether property log file has been initialized
+	int log_every;           // Add system properties to log every _ frames
+	FILE *log_file;          // Pointer to log file
 	
-	int nvt_on;
-	double zeta;
-	double tau_damp;
-	double T_des;
+	int nvt_on;          // Whether to use an NVT thermostat 
+	double zeta;         // Nose-Hoover thermodynamic friction coefficient
+	double tau_damp;     // Nose-Hoover dimensionless damping time constant
+	double T_des;        // Desired system temperature for NVT
 	
-}MDSystem;
-
-
+} MDSystem;
 
 #endif
