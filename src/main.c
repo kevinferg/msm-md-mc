92e14f4275968d652dd3ed4582ef8f475a2baae1
kevinferg <kevinferg98@gmail.com>
2022-06-27 16:56:50 -0400

    Add custom potential examples + separate examples folder

--- src/main.c
+++ src/main.c
@@ -3,7 +3,13 @@
 #include <math.h>
 #include "md_all.h"
 
+#include "../examples/custom_potential_ex.h"
+
 int main(int argc, char** argv) {
+
+	custom_potential_ex();
+
+	/*
 	// Export pair potential and force for a range of radii
 	MDSystem sys;
 	sys_init(&sys);
@@ -14,14 +20,15 @@ int main(int argc, char** argv) {
 	// Run a Monte Carlo simulation
 	mc_simulation("liquid256.txt", "mc_results.log", "mc_snapshot.xyz",
 	              5, 6.8, 0.83, 0.1,
-				  10000, 0, 0);
+				  100, 0, 0);
 	printf("MC: Success.\n\n");
 
 	// Run a molecular dynamics simulation
 	md_simulation("liquid256.txt", "md_results.log", "md_trajectory.xyz",
 	              5, 10, 0.002,
 				  6.8, 0.83, 1.77, 0.05,
-				  2500, 2500, 100, 0);
+				  100, 100, 10, 0);
 	printf("MD: Success.\n\n");
+	*/
 	return 0;
 }
