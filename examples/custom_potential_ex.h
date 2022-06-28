1f7e48690dbb106c0529e59d56e70765de9e1a89
kevinferg <kevinferg98@gmail.com>
2022-06-28 10:05:46 -0400

    Document custom potential example

--- examples/custom_potential_ex.h
+++ examples/custom_potential_ex.h
@@ -1,12 +1,4 @@
 
-double F_morse(double r, double* params) {
-    double De = params[0];   // Well depth
-    double a = params[1];    // Well width
-    double r_eq = params[2]; // Equilibrium bond distance
-    double part = exp(a * (r_eq - r));
-    return -2 * a * De * part * (1 - part);
-}
-
 double U_morse(double r, double* params) {
     double De = params[0];   // Well depth
     double a = params[1];    // Well width
@@ -15,13 +7,28 @@ double U_morse(double r, double* params) {
     return De * part * part;
 }
 
+double F_morse(double r, double* params) {
+    double De = params[0];   // Well depth
+    double a = params[1];    // Well width
+    double r_eq = params[2]; // Equilibrium bond distance
+    double part = exp(a * (r_eq - r));
+    return -2 * a * De * part * (1 - part);
+}
+
 int custom_potential_ex(void) {
-    MDSystem sys;
-    sys_init(&sys);
-    Potential morse;
-    potential_init(&morse, 2.5, 
-                   &U_morse, &F_morse,
-                   (double[3]) {1, 4, 1.1});
-    sys.potential = &morse;
+    MDSystem sys;                      // Create system
+    sys_init(&sys);                    // Initialize system 
+                                       // (with default LJ potential)
+    
+    Potential morse;                   // Create a new potential
+    potential_init(&morse,             // Assign the potential:
+                   2.5,                // - Cutoff radius: 2.5
+                   &U_morse, &F_morse, // - Energy and Force functions
+      (double[3])  {1, 4, 1.1});       // - Morse params: De, a, r_eq
+
+    sys.potential = &morse;            // Apply our new Morse potential
+                                       // to each pair in the system
+
+    /* Export [r, U, F] table to potential.log */
 	check_potential(&sys, "potential.log");
 }
