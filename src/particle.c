4a7d19ee765aa0e5337f568eabc1c3a7847eff92
kevinferg <kevinferg98@gmail.com>
2022-06-28 12:59:29 -0400

    Reformat particle.c/h

--- src/particle.c
+++ src/particle.c
@@ -5,63 +5,69 @@
 #include "particle.h"
 
 
-int pt_set_force(Particle *pt,Vec3 *vec){
+/*********** Set a particle property ***********/
+
+int pt_set_force(Particle *pt, Vec3 *vec) {
 	vec_copy(vec, &(pt->force));
-	return(0);
+	return 0;
 }
 
-int pt_set_pos(Particle *pt,Vec3 *vec){
-	vec_copy(vec,&(pt->pos));
-	return(0);
+int pt_set_pos(Particle *pt, Vec3 *vec) {
+	vec_copy(vec, &(pt->pos));
+	return 0;
 }
 
-int pt_set_vel(Particle *pt,Vec3 *vec){
+int pt_set_vel(Particle *pt, Vec3 *vec) {
 	vec_copy(vec, &(pt->vel));
-	return(0);
+	return 0;
 }
-int pt_set_mass(Particle *pt,double val){
+int pt_set_mass(Particle *pt, double val) {
 	pt->mass = val;
-	return(0);
+	return 0;
 }
 
-int pt_get_force(Particle *pt,Vec3 *vec){
-	vec_copy(&(pt->force),vec);
-	return(0);
+
+/*********** Get a particle property ***********/
+
+int pt_get_force(Particle *pt, Vec3 *vec) {
+	vec_copy(&(pt->force), vec);
+	return 0;
 }
 
-int pt_get_pos(Particle *pt,Vec3 *vec){
-	vec_copy(&(pt->pos),vec);
-	return(0);
+int pt_get_pos(Particle *pt, Vec3 *vec) {
+	vec_copy(&(pt->pos), vec);
+	return 0;
 }
 
-int pt_get_vel(Particle *pt,Vec3 *vec){
-	vec_copy(&(pt->vel),vec);
-	return(0);
+int pt_get_vel(Particle *pt, Vec3 *vec) {
+	vec_copy(&(pt->vel), vec);
+	return 0;
 }
 
-int pt_get_mass(Particle *pt,double *val){
+int pt_get_mass(Particle *pt, double *val) {
 	*val = pt->mass;
-	return(0);
+	return 0;
 }
 
-int pt_add_force(Particle *pt,Vec3 *vec){
+
+/*** Add a quantity onto a particle property ***/
+
+int pt_add_force(Particle *pt, Vec3 *vec) {
 	vec_add(vec, &(pt->force));
-	return(0);
+	return 0;
 }
 
-int pt_add_pos(Particle *pt,Vec3 *vec){
+int pt_add_pos(Particle *pt, Vec3 *vec) {
 	vec_add(vec, &(pt->pos));
-	return(0);
+	return 0;
 }
 
-int pt_add_trupos(Particle *pt,Vec3 *vec){
+int pt_add_trupos(Particle *pt, Vec3 *vec) {
 	vec_add(vec, &(pt->trupos));
-	return(0);
+	return 0;
 }
 
-
-int pt_add_vel(Particle *pt,Vec3 *vec){
+int pt_add_vel(Particle *pt, Vec3 *vec) {
 	vec_add(vec, &(pt->vel));
-	return(0);
+	return 0;
 }
-
