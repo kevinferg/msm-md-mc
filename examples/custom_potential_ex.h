ad77eeddae1e6688a776adc64603696cc7fd958e
kevinferg <kevinferg98@gmail.com>
2022-07-08 09:33:50 -0400

    Update readme with revised custom potential example

--- examples/custom_potential_ex.h
+++ examples/custom_potential_ex.h
@@ -17,23 +17,22 @@ double F_morse(double r, const void* params) {
 }
 
 int custom_potential_ex(void) {
-    MDSystem sys;                      // Create system
-    sys_init(&sys);                    // Initialize system 
-                                       // (with default LJ potential)
-
-    Potential morse;                   // Create a new potential
-    MorseParameters params = {         // Define Morse parameters
+    MDSystem sys;                       // Create system
+    sys_init(&sys);                     // Initialize system 
+                                        // (with default LJ potential)
+    MorseParameters params = {          // Define Morse parameters
         .D_e = 1, .a = 4, .r_e = 1.1
     };
-    potential_init(&morse,             // Assign the potential:
-                   2.5,                // - Cutoff radius: 2.5
-                   &U_morse, &F_morse, // - Energy and Force functions
-                   &params);           // - Morse params: D_e, a, r_e
+    Potential morse;                    // Create a new potential
+    potential_init(&morse,              // Assign the potential:
+                    2.5,                // - Cutoff radius: 2.5
+                    &U_morse, &F_morse, // - Energy and Force functions
+                    &params);           // - Morse params: D_e, a, r_e
 
-    sys.potential = &morse;            // Apply our new Morse potential
-                                       // to each pair in the system
+    sys.potential = &morse;             // Apply our new Morse potential
+                                        // to each pair in the system
 
     /* Export [r, U, F] table to potential.log */
-	check_potential(&sys, "potential.log");
+    check_potential(&sys, "potential.log");
     return 0;
 }
